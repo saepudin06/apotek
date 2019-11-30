8e7d793f203ac8e8c2b8dffa183b4d3e51e64ef3
saepudin06 <saepudin06@gmail.com>
2019-12-01 01:40:25 +0700

    ''

--- application/helpers/security_check_helper.php
+++ application/helpers/security_check_helper.php
@@ -5,7 +5,8 @@ function check_login($ws = '') {
 
 	if($ci->session->userdata('logged_in') == "") {
 		if($ci->input->is_ajax_request()) { //request from Web Service (ws.php)
-			throw new Exception('Sorry, Your login session has been expired. <br/> Please <a href="'.base_url().'auth/index">Login</a> first so that You can access this page. Thank You');
+			// throw new Exception('Sorry, Your login session has been expired. <br/> Please <a href="'.base_url().'auth/index">Login</a> first so that You can access this page. Thank You');
+			redirect('auth/logout');
 		}else {
 			redirect('auth/logout');
 		}
